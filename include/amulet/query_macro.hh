bdc09a7d791c1c6e41ab073180b448518d8a8981
iofg2100 <iofg2100@gmail.com>
2013-10-27 22:52:00 +0900

    refactor query macro

--- include/amulet/query_macro.hh
+++ include/amulet/query_macro.hh
@@ -15,29 +15,34 @@
 #define AMULET_SELECT(...) _AmuletMonadType::fromValue(__VA_ARGS__)
 
 // returns reference-removed decltype of expr
-#define AMULET_SAFE_DECLTYPE(expr) typename std::remove_reference<decltype(expr)>::type
+#define AMULET_GET_TYPE(expr) typename std::remove_reference<decltype(expr)>::type
 
-// if head = (AMULET_FROM_INDEX, x, xs)
-//   xs.flatMap([=](decltype(xs)::value_type x){
-//     using _AmuletMonadType = decltype(xs);
+// if (index, arg1, arg2) = (AMULET_FROM_INDEX, x, xs)
+//   xs.flatMap([=](const auto &x){
+//     using _AmuletMonadType = AMULET_GET_TYPE(xs);
 //     return tail;})
-// else if head = (AMULET_LET_INDEX, x, y)
-//   [=](decltype(y) x){return tail;}(y)
-#define AMULET_DO_FOLDER(d, tail, head) \
+// else if (index, arg1, arg2) = (AMULET_LET_INDEX, x, y)
+//   [=](const auto x){return tail;}(y)
+// else if (index, arg1, arg2) = (AMULET_WHERE_INDEX, expr, )
+//   (tail).filter([=](const auto &){return expr;})
+#define AMULET_DO_FOLDER_IMPL(index, arg1, arg2, tail) \
   BOOST_PP_IF( \
-    BOOST_PP_EQUAL(BOOST_PP_TUPLE_ELEM(3,0,head), AMULET_FROM_INDEX), \
-    BOOST_PP_TUPLE_ELEM(3,2,head).flatMap([=](const AMULET_SAFE_DECLTYPE(BOOST_PP_TUPLE_ELEM(3,2,head))::value_type & BOOST_PP_TUPLE_ELEM(3,1,head)) { \
-      using _AmuletMonadType = AMULET_SAFE_DECLTYPE(BOOST_PP_TUPLE_ELEM(3,2,head)); \
+    BOOST_PP_EQUAL(index, AMULET_FROM_INDEX), \
+    arg2.flatMap([=](const AMULET_GET_TYPE(arg2)::value_type & arg1) { \
+      using _AmuletMonadType = AMULET_GET_TYPE(arg2); \
       return tail; }),\
     BOOST_PP_IF( \
-      BOOST_PP_EQUAL(BOOST_PP_TUPLE_ELEM(3,0,head), AMULET_LET_INDEX), \
-      [=](const AMULET_SAFE_DECLTYPE(BOOST_PP_TUPLE_ELEM(3,2,head)) & BOOST_PP_TUPLE_ELEM(3,1,head)){ return tail; }(BOOST_PP_TUPLE_ELEM(3,2,head)), \
+      BOOST_PP_EQUAL(index, AMULET_LET_INDEX), \
+      [=](const AMULET_GET_TYPE(arg2) & arg1){ return tail; }(arg2), \
       BOOST_PP_IF( \
-        BOOST_PP_EQUAL(BOOST_PP_TUPLE_ELEM(3,0,head), AMULET_WHERE_INDEX), \
-        (tail).filter([=](const AMULET_SAFE_DECLTYPE(tail)::value_type &){return BOOST_PP_TUPLE_ELEM(3,1,head);}), \
+        BOOST_PP_EQUAL(index, AMULET_WHERE_INDEX), \
+        (tail).filter([=](const AMULET_GET_TYPE(tail)::value_type &){return arg1;}), \
       ) \
     ) \
   )
+
+#define AMULET_DO_FOLDER(d, tail, head) \
+  (AMULET_DO_FOLDER_IMPL(BOOST_PP_TUPLE_ELEM(3,0,head), BOOST_PP_TUPLE_ELEM(3,1,head), BOOST_PP_TUPLE_ELEM(3,2,head), tail))
 
 #define AMULET_DO_REVERSED_LIST(reversed_list) \
   BOOST_PP_LIST_FOLD_LEFT( \
